3f3169db3cfd80a60b6b7c349fc62c4651345085
Zokky2e <icro4.gaming@hotmail.com>
2023-05-28 19:27:49 +0200

    added simple drawing board

--- app/src/main/java/com/example/drawing_board_rma/MainActivity.kt
+++ app/src/main/java/com/example/drawing_board_rma/MainActivity.kt
@@ -3,32 +3,41 @@ package com.example.drawing_board_rma
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Path
+import com.example.drawing_board_rma.ui.components.DrawingCanvas
 import com.example.drawing_board_rma.ui.theme.DrawingboardrmaTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    private val action: MutableState<Pair<Boolean, Pair<Float, Float>>?> = mutableStateOf(null)
+    private val path = Path()
+
+    private val collectList = mutableListOf<Pair<Boolean, Pair<Float, Float>>>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DrawingboardrmaTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column() {
+                        Button(onClick = { collectList.clear()}) {
+                            Text(text = "Clear")
+                        }
+                        DrawingCanvas(action = action, path = path, collectList=collectList)
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
 }
---
+++ app/src/main/java/com/example/drawing_board_rma/ui/components/DrawingCanvas.kt
@@ -0,0 +1,73 @@
+package com.example.drawing_board_rma.ui.components
+
+import android.view.MotionEvent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import com.example.drawing_board_rma.ui.theme.Pink40
+import com.example.drawing_board_rma.ui.theme.Pink80
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun DrawingCanvas(
+    action: MutableState<Pair<Boolean, Pair<Float, Float>>?>,
+    path: Path,
+    collectList: MutableList<Pair<Boolean, Pair<Float, Float>>>,
+modifier: Modifier = Modifier,
+) {
+
+    Canvas(modifier = modifier
+        .fillMaxSize()
+        .pointerInteropFilter {
+        when (it.action) {
+            MotionEvent.ACTION_DOWN -> {
+                action.value = Pair(true, Pair(it.x, it.y))
+                path.moveTo(it.x, it.y)
+                collectList.add(Pair(true, Pair(it.x, it.y)))
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                action.value = Pair(false, Pair(it.x, it.y))
+                path.lineTo(it.x, it.y)
+                collectList.add(Pair(false, Pair(it.x, it.y)))
+            }
+
+            MotionEvent.ACTION_UP -> {
+
+            }
+
+            else -> false
+
+        }
+        true
+    }) {
+        action.value?.let {
+            drawPath(
+                path = collectList.toPath(),
+                color = Pink80,
+                alpha = 1f,
+                style = Stroke(10f)
+            )
+        }
+    }
+}
+
+fun MutableList<Pair<Boolean, Pair<Float, Float>>>.toPath(): Path {
+    val path = Path()
+    forEach {
+        if(it.first) {
+            path.moveTo(it.second.first, it.second.second)
+        } else {
+            path.lineTo(it.second.first, it.second.second)
+        }
+    }
+
+    return path
+}
